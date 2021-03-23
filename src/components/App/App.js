04d48f20c20b16dc4f8b360e7cea1f0ae7d59384
Shyamal <sgprajapati24@gmail.com>
2021-03-23 11:56:42 +0530

    First Component Complete
    Pro-1 Part-6 Complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
-meenakshyv21
+import Header from '../Header/Header'
+import './App.css'
+
 class App extends React.Component {
 render() {
+return (
 <div>
-      <h1> AAM - An App a Month </h1>
-    </div>
+  <Header/>    
+</div>
+);
 }
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css';
+import icon from './first.svg';
+
+const Header =()=>{
+    return( 
+        <div className="head-container">
+        <img src={icon}
+        alt="HeaderImage" 
+        className="head-img" />
+        {/* Implemented new img for learning purpose */}
+        {/* <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
+        alt="HeaderImage" 
+        className="head-img" /> */}
+
+        <h1 className="head-text">Name it</h1>
+        </div>
+    );
+}
+
+export default Header;
