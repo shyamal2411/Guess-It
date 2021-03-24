caa7e9017dbfb10a00590bc49cb481c1316fc7ab
Shyamal <sgprajapati24@gmail.com>
2021-03-24 12:16:22 +0530

    State & Props
    Learnt using  State & props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './App.css'
+import SearchBox from '../SearchBox/SearchBox';
 
 class App extends React.Component {
-render() {
+
+    constructor() {
+      super();
+
+      this.state = {
+      headerText:'Guess it!',
+    };
+  }
+
+  render() {
 return (
 <div>
-  <Header/>    
+  <Header headtitle={this.state.headerText}/>    
+  <SearchBox/>
 </div>
 );
 }
