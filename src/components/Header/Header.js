ffb6ec09512f10bb0fff20790b4c0b599f9c4454
Shyamal <sgprajapati24@gmail.com>
2021-03-24 19:05:59 +0530

    Header transition, setting props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,37 @@ import React from 'react'
 import './Header.css';
 import icon from './first.svg';
 
-const Header =(props)=>{
+
+
+const Header =({headTitle,headerExpanded})=>{
     return( 
         <div className="head-container">
-        <img src={icon}
+        <img 
+        src={icon}
         alt="HeaderImage" 
-        className="head-img" />
-        {/* Implemented new img for learning purpose */}
+        className={`head-image ${
+                    headerExpanded
+                        ? 'head-image-expanded'
+                        : 'head-image-contracted'
+                }`}
+                alt="headerImage"
+            />
+            <h1
+                className={`head-text ${
+                    headerExpanded
+                        ? 'head-text-expanded'
+                        : 'head-text-contracted'
+                }`}
+            >
+                {headTitle}
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
+
+ {/* Implemented new img for learning purpose */}
         {/* <img src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
         alt="HeaderImage" 
         className="head-img" /> */}
-
-        <h1 className="head-text">{props.headtitle}</h1>
-        </div>
-    );
-}
-
-export default Header;
