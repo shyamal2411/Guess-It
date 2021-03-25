7d1082194022cd5be1b101241e22d059e86b6b9c
Shyamal <sgprajapati24@gmail.com>
2021-03-25 09:46:27 +0530

    Changed headerExpand

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,7 +24,7 @@ const Header =({headTitle,headerExpanded})=>{
                         : 'head-text-contracted'
                 }`}
             >
-                {headTitle}
+                Guess It!
             </h1>
         </div>
     );
